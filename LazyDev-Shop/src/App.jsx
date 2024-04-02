0532f11f599b891df43e80b00fc82a4df85c5d63
Irene Sánchez <enerisan@gmail.com>
2024-04-02 17:41:16 +0200

    commit

---
+++ LazyDev-Shop/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Product from "Product.Catalog.jsx"
+import ProductCatalog from "ProductCatalog.jsx"
+import Product
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+        
+    </>
+  )
+}
+
+export default App
