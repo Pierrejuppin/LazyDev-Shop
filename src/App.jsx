0b1d3a5dd5f5e0a4be0985c6aae1b9737affe4cd
Pierre Juppin <pierre.juppin@gmail.com>
2024-04-02 16:45:13 +0200

    Feat: Adding Array products

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,70 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import Product from "Product.jsx"
+import ProductCatalog from "ProductCatalog.jsx"
+import ProductFilter from "ProductFilter.jsx"
+import ProductDetails from "ProductDetails.jsx"
+
+const products = [
+  {
+      name: "WC portatif",
+      image: "./assets/",
+      price: "1900€",
+      category: "Coup de coeur"
+  },
+  {
+    name: "Livre: Web pour les Nulls",
+    image: "./assets/",
+    price: "9,90€",
+    category: "utile"
+},
+{
+  name: "Correcteur de posture",
+  image: "./assets/",
+  price: "19,90€",
+  category: "utile"
+},
+{
+  name: "Pince attrape tout",
+  image: "./assets/",
+  price: "75€",
+  category: "indispensable"
+},
+{
+  name: "Canapé Desk",
+  image: "./assets/",
+  price: "47,50€",
+  category: "utile"
+},
+{
+  name: "Tapis sans fil",
+  image: "./assets/",
+  price: "39,99€",
+  category: "indispensable"
+},
+  
+]
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ProductFilter />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <ProductCatalog />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <ProductDetails />
+      </div>
+        <div>
+          <Product />
+        </div>
     </>
   )
 }
